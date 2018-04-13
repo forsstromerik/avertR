30a556020fb413548a9329c3908478b2cdb20287
TheresePetersson <therese-petersson@live.se>
2018-04-13 21:55:18 +0200

    Added basic API

--- backend/models/disturbance.js
+++ backend/models/disturbance.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 var DisturbanceSchema = new mongoose.Schema({
     lat: { type: Number, required: true },
     lon: { type: Number, required: true },
-    timestamp: { type: Date, required: true },
-    notes: { type: String, required: true },
-    status: { type: String, required: true }
+    timestamp: { type: Date, default: Date.now },
+    notes: { type: String },
+    status: { type: String, default: "PENDING" }
 });
 
 module.exports = mongoose.model('Disturbance', DisturbanceSchema);
--- backend/routes/base.js
+++ backend/routes/base.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+var Disturbance = require('../models/disturbance');
 
 router.get('/', function(req, res) {
-    res.json({
+    /*res.json({
         name: 'disturbance-reporter-api',
         status: 'running'
-    });
+    });*/
+    var ds = new DisturbanceSchema(lat = 0.8, lon = 0.1);
+    res.json(ds)
 });
-
+ 
 module.exports = router;
