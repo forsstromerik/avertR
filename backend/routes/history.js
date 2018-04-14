47cb3344afd68b7a8caf94cbc60ba6fbbc8f7ee8
Axel Niklasson <axel.niklasson@live.com>
2018-04-14 12:30:02 +0200

    stats

--- backend/routes/history.js
+++ backend/routes/history.js
@@ -5,65 +5,65 @@ const keywords = require('../etc/suggestions-keywords');
 const helpers = require('../etc/helpers');
 
 let convert_to_days ={
-	0 : "SUNDAY",
-	1 : "MONDAY",
-	2 : "TUESDAY",
-	3: "WEDNESDAY",
-	4: "THURSDAY",
-	5: "FRIDAY",
-	6: "SATURDAY"
+    0 : "Sunday",
+    1 : "Monday",
+    2 : "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday"
 }
-
-let days ={
-	"SUNDAY": 0,
-	"MONDAY": 0,
-	"TUESDAY": 0,
-	"WEDNESDAY": 0,
-	"THURSDAY": 0,
-	"FRIDAY": 0,
-	"SATURDAY": 0
-}
-
-let hours = {
-}
-
-let location = [];
 
 router.get('/', function(req, res) {
     Disturbance.find({status : "ACTIVE"}).sort({timestamp: -1}).exec(function(err, disturbances) {
-    	if(err){
-    		res.status(500).send({error: "Could not load disturbances"});
-    	}else{
-    		disturbances.map(d => {
-    			let date = new Date(d.timestamp);
-    			let hour = date.getHours();
-    			let day = date.getDay();
-    			days[convert_to_days[day]]++;
+	if(err){
+	    res.status(500).send({error: "Could not load disturbances"});
+	}else{
+	    let days ={
+		"Monday": 0,
+		"Tuesday": 0,
+		"Wednesday": 0,
+		"Thursday": 0,
+		"Friday": 0,
+		"Saturday": 0,
+		"Sunday": 0
+	    }
 
-    			if(hours[hour] > 0){
-    				hours[hour]++;
-    			}else{
-    				hours[hour] = 1;
-    			}
-    			let pos = {'latitude' : d.lat, 'longitude': d.lon};
-    			let grouped = false;
-				for (var i = 0; i < location.length; i++) { 
-    				let pos_group = {'latitude' : location[i].latitude, 'longitude': location[i].longitude};
-				    if(helpers.withinRadius(pos_group, pos , 0.5)){
-				    	location[i].events++;
-				    	grouped = true;
-				    }
-				}
-				if(!grouped){
-					let new_loc = {'latitude' : d.lat, 'longitude': d.lon, 'events': 1};
-					location.push(new_loc);
-				}
-    		});
+	    let hours = {
+	    }
 
-    		res.json({ 'days': days, 'hours': hours, 'locations':location});
-    	}
+	    let location = [];
+
+	    disturbances.map(d => {
+		let date = new Date(d.timestamp);
+		let hour = date.getHours();
+		let day = date.getDay();
+		days[convert_to_days[day]]++;
+
+		if(hours[hour] > 0){
+		    hours[hour]++;
+		}else{
+		    hours[hour] = 1;
+		}
+		let pos = {'latitude' : d.lat, 'longitude': d.lon};
+		let grouped = false;
+		for (var i = 0; i < location.length; i++) { 
+		    let pos_group = {'latitude' : location[i].latitude, 'longitude': location[i].longitude};
+		    if(helpers.withinRadius(pos_group, pos , 0.5)){
+			location[i].events++;
+			grouped = true;
+		    }
+		}
+		if(!grouped){
+		    let new_loc = {'latitude' : d.lat, 'longitude': d.lon, 'events': 1};
+		    location.push(new_loc);
+		}
+	    });
+
+	    res.json({ 'days': days, 'hours': hours, 'locations':location});
+	}
     });
 });
- 
+
 module.exports = router;
 
--- frontend/admin/src/components/Insights.js
+++ frontend/admin/src/components/Insights.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
 
 class Insights extends Component {
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      historyData: null
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/history')
+      .then(res => {
+        return res.json()
+      }).then(body => {
+        this.setState({ historyData: body });
+      })
+  }
+
+  renderChart() {
+    var ctx = document.getElementById("daysChart").getContext('2d');
+    var myChart = new window.Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: Object.keys(this.state.historyData.days),
+        datasets: [{
+          label: '# of disturbances',
+          data: Object.values(this.state.historyData.days),
+          backgroundColor: 'rgba(31, 188, 211, 0.2)',
+          borderColor: '#1FBCD3',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true
+            }
+          }]
+        }
+      }
+    }); 
   }
 
   render() {
+    if (this.state.historyData) {
+      this.renderChart();
+    }
+
     return (
-      <h1>Insights</h1>
+      <canvas id="daysChart" width="400" height="400"></canvas>
     );
   }
 }
