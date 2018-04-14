5712a42adc62df596bdb7dc7be76cf117a883306
Axel Niklasson <axel.niklasson@live.com>
2018-04-14 14:53:25 +0200

    add object to get

--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -12,7 +12,7 @@ router.get('/suggestions', function(req, res) {
         }else{
             suggestions = []; 
             disturbances.map((el1) => {
-                var group_suggestion = [ el1._id ];
+                var group_suggestion = [ el1 ];
                 disturbances.map((el2) => {
 
                     if(el1._id !== el2._id){
@@ -20,7 +20,7 @@ router.get('/suggestions', function(req, res) {
                         let el2_pos = {latitude : el2.lat, longitude : el2.lon};
 
                         if(helpers.withinRadius(el1_pos, el2_pos, 1) && withinTimeFrame(el1.timestamp, el2.timestamp, 1200000) && withinCorrelation(el1.notes, el2.notes, 0.4) ){
-                            group_suggestion.push(el2._id);
+                            group_suggestion.push(el2);
                         }
                     }
                 });
@@ -28,14 +28,14 @@ router.get('/suggestions', function(req, res) {
                 if(group_suggestion.length > 1){
                     let match = false;
                     suggestions.map(el => {
-                      if(el.sort().join(',')=== group_suggestion.sort().join(',')){
-                        match = true;
-                      }
+                        if(el.sort().join(',')=== group_suggestion.sort().join(',')){
+                            match = true;
+                        }
                     });
 
-                  if (!match) {
+                    if (!match) {
                         suggestions.push(group_suggestion);
-                  }
+                    }
                 }
             });
 
@@ -46,22 +46,22 @@ router.get('/suggestions', function(req, res) {
 });
 
 router.get('/', function(req, res) {
-    Group.find({}).sort({timestamp: -1}).exec(function(err, groups) {
-    	if(err){
-    		res.status(500).send({error: "Could not load groups"});
-    	}else{
-    		res.json(groups);
-    	}
+    Group.find({}).sort({timestamp: -1}).populate('disturbances').exec(function(err, groups) {
+        if(err){
+            res.status(500).send({error: "Could not load groups"});
+        }else{
+            res.json(groups);
+        }
     });
 });
 
 router.post('/', function(req, res) {
-	let disturbances = req.body.disturbances;
+    let disturbances = req.body.disturbances;
 
-	Group.create({disturbances: disturbances}, function(err, group){
-		if(err){
-			res.status(500).send({error: "Could not create group" });
-		}else{
+    Group.create({disturbances: disturbances}, function(err, group){
+        if(err){
+            res.status(500).send({error: "Could not create group" });
+        }else{
             disturbances.map(d_id => {
                 Disturbance.update({_id: d_id},{$set: {groupId: group._id }}, function(err, disturbance){
                     if(err){
@@ -69,43 +69,44 @@ router.post('/', function(req, res) {
                     }
                 });
             });
-			res.json(group);
-		}
-	});
+
+            res.json(group);
+        }
+    });
 });
 
 router.get('/:id', function(req, res) {
-    Group.findById(req.params.id, function(err, group){
-    	if(err){
-    		res.status(500).send({error: "Could not find group " });
-    	}else{
-    		res.json(group);
-    	}
+    Group.findById(req.params.id).populate('disturbances').exec(function(err, group){
+        if(err){
+            res.status(500).send({error: "Could not find group " });
+        }else{
+            res.json(group);
+        }
     });
 });
 
 router.put('/:id', function(req, res) {
-	let notes = req.body.notes;
-	let id = req.params.id;
+    let notes = req.body.notes;
+    let id = req.params.id;
 
-	Group.update({ _id: id}, { $set: { notes: notes }}, function(err, group) {
-		if(err){
-			res.status(500).send({error: "Could not update group" });
-		}else{
-			res.json(group);
-		}
-	});
+    Group.update({ _id: id}, { $set: { notes: notes }}, function(err, group) {
+        if(err){
+            res.status(500).send({error: "Could not update group" });
+        }else{
+            res.json(group);
+        }
+    });
 });
 
 router.delete('/:id', function(req, res) {
     let id = req.params.id;
 
     Group.remove({ _id: id}, function(err){
-    	if(err){
-    		res.status(500).send({error: "Could not delete group" });
-    	}else{
-    		res.sendStatus(200);
-    	}
+        if(err){
+            res.status(500).send({error: "Could not delete group" });
+        }else{
+            res.sendStatus(200);
+        }
     });
 });
 
@@ -117,14 +118,14 @@ function withinCorrelation(notes1, notes2, correlation_limit){
     let matches = 0;
 
     s1.map((word) =>{
-      if(keywords[word] > 0){
+        if(keywords[word] > 0){
             groups.push(keywords[word]);
         }
     });
 
     s2.map((word) =>{
         if(keywords[word] > 0 && groups.indexOf(keywords[word]) !== -1){
-                matches++;
+            matches++;
         }
     });
 
@@ -132,10 +133,10 @@ function withinCorrelation(notes1, notes2, correlation_limit){
     return(correlation > correlation_limit);
 }
 function withinTimeFrame(time1, time2, msec){
-        let d1 = Date.parse(time1);
-        let d2 = Date.parse(time2);
-        let diff = Math.abs(d1-d2);
-        return (diff <= msec);
+    let d1 = Date.parse(time1);
+    let d2 = Date.parse(time2);
+    let diff = Math.abs(d1-d2);
+    return (diff <= msec);
 }
 
 
